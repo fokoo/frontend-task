52fd53019d661004a85660899c133c39a7d9c983
fokoo <fokofotso@gmail.com>
2021-08-08 00:50:03 +0200

    object Question added

---
+++ src/app/models/Question.ts
@@ -0,0 +1,6 @@
+export class Question {
+  id: number = 0;
+  question: string = "";
+  response: string = "";
+  choices: string[] = [];
+}
---
+++ src/app/models/test.ts
@@ -0,0 +1,11 @@
+export class Test {
+  name: string = "no-name";
+  result: number = 0;
+  duration: number = 100;
+  date: string = ""
+
+   // constructor(public name?: string, public result?: string,
+   //             public duration?: string) {
+   //     this.date = "";
+   // }
+}
