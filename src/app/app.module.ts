bb19e1caa0ff2fecba63b5e4ae1b68152fb7c5ef
fokoo <fokofotso@gmail.com>
2021-08-08 00:57:55 +0200

    app module set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {RouterModule, Routes} from "@angular/router";
+import {DashboardService} from "./services/dashboard.service";
+import {HttpClientModule} from "@angular/common/http";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
+const appRoutes: Routes = [
+  {
+    path: '', component: DashboardComponent, canActivate: [] },
+  ];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
+    imports: [
+        BrowserModule,
+        RouterModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes),
+    ],
+    exports: [RouterModule],
+  providers: [DashboardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
