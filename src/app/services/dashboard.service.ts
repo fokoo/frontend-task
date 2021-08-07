d9027d3bc49f6fc1982394406886aaba888326dc
fokoo <fokofotso@gmail.com>
2021-08-08 00:51:27 +0200

    api added in service

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
+  host = 'http://localhost:3000';
+  constructor(private httpClient: HttpClient) {
+  }
 
-  constructor() { }
+  getQuestions(): Observable<any> {
+    return this.httpClient.get(this.host);
+  }
 }
