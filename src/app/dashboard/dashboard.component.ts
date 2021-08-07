d69346f011f13c0e7a55d9342c8246a904663835
fokoo <fokofotso@gmail.com>
2021-08-08 00:56:52 +0200

    dashboard implementation (button to start test, needed parameters and methods set, method to call question, api..)

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Test} from "../models/test";
+import {DashboardService} from "../services/dashboard.service";
+import {Question} from "../models/Question";
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  choices: any;
+  questionInfo: any;
+  time: any;
+  tests: Test[] = [];
+  question: Question = new Question();
+  questions: Question[] = [];
+  index = 0;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(private dashboardService :DashboardService) {
   }
 
+  ngOnInit(): void {
+    this.dashboardService.getQuestions().subscribe(
+      (questions: Array<Question>) => {
+      this.questions = questions;
+      this.setNewQuestion();
+     })
+  }
+
+  setNewQuestion(){
+    this.question = this.questions[this.index++%4];
+  }
+  nextQuestion() {
+    this.setNewQuestion();
+    // check if good answers
+    // calcule current result
+  }
+
+  startNewTest() {
+    this.ngOnInit();
+  }
 }
